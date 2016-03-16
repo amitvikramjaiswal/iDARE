324c5ea725426e1f2e4f556826c54ac3471a33a8
Amit Vikram Jaiswal <amitvikramjaiswal@gmail.com>
2016-03-16 20:37:18 +0530

    BaseActivity

--- app/src/main/java/com/opensource/app/idare/view/activities/BaseActivity.java
+++ app/src/main/java/com/opensource/app/idare/view/activities/BaseActivity.java
@@ -1,8 +1,16 @@
 package com.opensource.app.idare.view.activities;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.opensource.app.idare.view.views.BaseView;
 
 /**
@@ -10,8 +18,68 @@ import com.opensource.app.idare.view.views.BaseView;
  */
 public class BaseActivity extends AppCompatActivity implements BaseView {
 
+    private static final String TAG = "BaseActivity";
+    private ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public String[] getStringArray(int arrayResourceId) {
+        return getResources().getStringArray(arrayResourceId);
+    }
+
+    @Override
+    public boolean checkGooglePlayServices() {
+        int errorCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getApplicationContext());
+        if (errorCode == ConnectionResult.SUCCESS) {
+            return true;
+        } else {
+            Dialog d = GoogleApiAvailability.getInstance().getErrorDialog(this, errorCode, 0);
+            if (d != null) {
+                d.show();
+            } else {
+                showToast("Could not initialize Google Play Services.");
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public void showProgressDialog(String status) {
+        hideProgressDialog();
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage(status);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.show();
+    }
+
+    @Override
+    public void hideProgressDialog() {
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void hideKeyBoard() {
+        InputMethodManager inputMethodManager = (InputMethodManager) this
+                .getSystemService(INPUT_METHOD_SERVICE);
+        View currentFocus = this.getCurrentFocus();
+        if (currentFocus != null) {
+            IBinder windowToken = this.getCurrentFocus().getWindowToken();
+            if (windowToken != null) {
+                inputMethodManager.hideSoftInputFromWindow(windowToken, 0);
+            }
+        }
     }
 }
--- app/src/main/java/com/opensource/app/idare/view/views/BaseView.java
+++ app/src/main/java/com/opensource/app/idare/view/views/BaseView.java
@@ -1,7 +1,98 @@
 package com.opensource.app.idare.view.views;
 
+import android.os.Bundle;
+import android.text.Spanned;
+
 /**
  * Created by ajaiswal on 3/15/2016.
  */
 public interface BaseView {
+
+    /**
+     * Shows a toast message.
+     *
+     * @param message The message to show.
+     */
+    void showToast(String message);
+
+    /**
+     * Shows or hides the indefinite progress bar.
+     *
+     * @param shown Whether the progress bar should be shown.
+     */
+    void setProgressBarIndeterminateVisibility(boolean shown);
+
+    /**
+     * Sets the app title.
+     *
+     * @param title The title.
+     */
+    void setTitle(int title);
+
+    /**
+     * Sets the app title.
+     *
+     * @param title The title.
+     */
+    void setTitle(CharSequence title);
+
+    /**
+     * Gets a string from app resources.
+     *
+     * @param stringResourceId The string resource id.
+     * @return The string.
+     */
+    String getString(int stringResourceId);
+
+    /**
+     * Gets a string array from app resources.
+     *
+     * @param arrayResourceId The array resource id.
+     * @return The string array.
+     */
+    String[] getStringArray(int arrayResourceId);
+
+    /**
+     * Closes the view.
+     */
+    void finish();
+
+    /**
+     * Checks if Google Play Services is available.
+     */
+    boolean checkGooglePlayServices();
+
+    /**
+     * Checks if the view is currently finishing.
+     */
+    boolean isFinishing();
+
+    /**
+     * Shows an alert dialog to prompt the user.
+     *
+     * @param title                  The title of the dialog, optional.
+     * @param message                The message for the dialog.
+     * @param itemlabels             If multiple choice, the item labels.
+     * @param checkItemLabelsChecked If multiple choice, the checked state of each item.
+     * @param positiveButton         The positive button (optional).
+     * @param negativeButton         The negative button (optional).
+     * @param alertDialogHandler     The dialog handler for button clicks.
+     */
+//    void showAlertDialog(String title, String message, Spanned[] itemlabels, boolean[] checkItemLabelsChecked, String positiveButton,
+//                         String negativeButton, AlertDialogHandler alertDialogHandler);
+
+    /**
+     * Shows a progress dialog in the center of the screen.
+     *
+     * @param status The status message to show.
+     */
+    void showProgressDialog(String status);
+
+    /**
+     * Hides the progress dialog from the center of the screen.
+     */
+    void hideProgressDialog();
+
+    void hideKeyBoard();
+
 }

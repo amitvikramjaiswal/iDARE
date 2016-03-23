b22afb930aca00ddd728befa5ebfe551925a8ac1
Amit Vikram Jaiswal <amitvikramjaiswal@gmail.com>
2016-03-23 16:21:09 +0530

    Fabric integrated

--- app/src/main/java/com/opensource/app/idare/application/IDareApp.java
+++ app/src/main/java/com/opensource/app/idare/application/IDareApp.java
@@ -3,7 +3,10 @@ package com.opensource.app.idare.application;
 import android.content.Context;
 import android.support.multidex.MultiDexApplication;
 
+import com.crashlytics.android.Crashlytics;
 import com.opensource.app.idare.util.log.Logger;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by ajaiswal on 3/16/2016.
@@ -13,11 +16,19 @@ public class IDareApp extends MultiDexApplication {
     private static final String TAG = "IDareApp";
     private static Context context;
 
+    public static Context getContext() {
+        return context;
+    }
+
+    public static void setContext(Context context) {
+        IDareApp.context = context;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         setContext(getApplicationContext());
-
+        Fabric.with(this, new Crashlytics());
         final Thread.UncaughtExceptionHandler handler = Thread.getDefaultUncaughtExceptionHandler();
         Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
@@ -29,13 +40,5 @@ public class IDareApp extends MultiDexApplication {
             }
         });
 
-    }
-
-    public static Context getContext() {
-        return context;
-    }
-
-    public static void setContext(Context context) {
-        IDareApp.context = context;
     }
 }
